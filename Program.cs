55039c4b193e712f9b18b933323555a6314385a4
Hannah Papa <hmfloyd16@gmail.com>
2023-02-28 10:14:38 -0600

    :inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Magic_Moose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
